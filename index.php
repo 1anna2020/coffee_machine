c6ead26e4e4f9d0c32edbe7cd5070c580e9d5e08
1anna2020 <strix1985@mail.ru>
2020-03-25 10:36:22 +0300

    Сделана оболочка

--- index.php
+++ index.php
@@ -15,38 +15,56 @@
     <div class="container">
       <div class="row coffee-machine">
         <div class="col-6 coffee-list">
-          <div class="row flex-column p-3">
-            <div class="coffee-item col">
-              <img src="img/americano.png" alt="">
-              <span>Американо - 50 руб.</span>
-            </div>
-            <div class="coffee-item col">
-              <img src="img/cappuccino.png" alt="">
-              <span>Капучино - 78 руб.</span>
-            </div>
-            <div class="coffee-item col">
-              <img src="img/espresso.png" alt="">
-              <span>Эспрессо - 21 руб.</span>
-            </div>
-            <div class="coffee-item col">
-             <img src="img/latte.jpg" alt="">
-             <span>Латтэ - 115 руб.</span>
-            </div>
+          <div class="row flex-column p-3 h-100 justify-content-around">
+                  <div class="coffee-item col">
+                    <img src="img/americano.png" alt="">
+                    <span>Американо - 50 руб.</span>
+                  </div>
+                  <div class="coffee-item col">
+                    <img src="img/cappuccino.png" alt="">
+                    <span>Капучино - 78 руб.</span>
+                  </div>
+                  <div class="coffee-item col">
+                    <img src="img/espresso.png" alt="">
+                    <span>Эспрессо - 21 руб.</span>
+                  </div>
+                  <div class="coffee-item col">
+                   <img src="img/latte.jpg" alt="">
+                   <span>Латтэ - 115 руб.</span>
+                  </div>
           </div>
         </div>
         <div class="col-6 coffee-oper">
           <div class="row p-3">
-            <div class="col-6">
-              <div class="display">
-                <p class="display-text">Выберите кофе</p>
-                <div class="progress">
-                  <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%">
-                  </div>
-                </div>
+              <div class="col-6">
+                <div class="display">
+                  <p class="display-text">Выберите кофе</p>
+                  
+                          <div class="progress">
+                              <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%">
+                                </div>
+                              </div>
+                          </div>
+                          <div class="coffee-cup">
+                            <img src="img/americano.png" alt="">
+                          </div>
+                
               </div>
-            </div>
-            <div class="col-6">
-            </div>
+              <div class="col-6">
+                          <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Баланс">
+                            <div class="input-group-append">
+                                <span class="input-group-text" id="basic-addon2">&#8381</span>
+                            </div>
+                          </div>
+                          <div class="atm">
+                            <img src="img/bill_acc.png" alt="">
+                          </div>
+                  <button class="btn btn-primary btn-block mt-2">Сдача</button>
+                  <div class="change-box">
+                    
+                  </div>
+              </div>
           </div>
         </div>
       </div>
